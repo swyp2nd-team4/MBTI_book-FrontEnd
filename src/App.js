6566ffb293e421d8cc82e468088e3baeb3adccd0
DESKTOP-LTPLH83\USER <wnsgur1855@naver.com>
2023-12-12 15:48:31 +0900

    [feat] | 개발세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Rechart from './Rechart';
+import Router from './shared/Router';
 
 function App() {
   return (
     <div style={{ width: 1000, height: 800 }}>
       <Rechart />
+      <Router />
     </div>
   );
 }
