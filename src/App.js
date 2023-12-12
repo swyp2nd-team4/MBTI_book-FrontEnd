c65589f7f70e65128581f9e9e31c4b602c25c566
DESKTOP-LTPLH83\USER <wnsgur1855@naver.com>
2023-12-12 15:38:39 +0900

    [feat] |  개발 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Rechart from './Rechart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 1000, height: 800 }}>
+      <Rechart />
     </div>
   );
 }
